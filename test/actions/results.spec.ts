57d5e8afcd01c6c9548bcb09d420178f9b6d9832
Matt Conrad <mmconrad@gmail.com>
2017-10-13 06:17:10 -0700

    Improve unit testing on results actions

--- test/actions/results.spec.ts
+++ test/actions/results.spec.ts
@@ -13,12 +13,19 @@ const emptyResultsSet = {
 };
 
 describe('actions', () => {
-  it('should saveResults should create saveResults action', () => {
-    expect(actions.saveResults(emptyResultsSet)).toMatchSnapshot();
+  // it('should saveResults should create saveResults action', () => {
+  //   expect(actions.saveResults(emptyResultsSet)).toMatchSnapshot();
+  // });
+  // it('should saveResultsInFile should create saveResultsInFile action', () => {
+  //   expect(actions.saveResultsToFile()).toMatchSnapshot();
+  // });
+
+  it('should exportResults should create exportResults action', () => {
+    expect(actions.exportResults()).toMatchSnapshot();
   });
 
-  it('should saveResultsInFile should create saveResultsInFile action', () => {
-    expect(actions.saveResultsToFile()).toMatchSnapshot();
+  it('should toggleShowResult should create toggleShowResult action', () => {
+    expect(actions.toggleShowResult(1)).toMatchSnapshot();
   });
 
   it('should setResults should create saveResults action', () => {
@@ -29,6 +36,59 @@ describe('actions', () => {
     expect(dispatch.calledWith({ 
       type: actions.saveResults.type, 
       payload: emptyResultsSet
+    })).toBe(true);
+  });
+
+  it('should toggleShowSearchResultRows should create toggleShowResult action', () => {
+    const fn = actions.toggleShowSearchResultRows(1);
+    expect(fn).toBeInstanceOf(Function);
+    const dispatch = spy();
+    fn(dispatch);
+    expect(dispatch.calledWith({
+      type: actions.toggleShowResult.type, 
+      payload: 1
+    })).toBe(true);
+  });
+
+  it('should showResultOverlay should create showResultOverlay action', () => {
+    const fn = actions.showResultOverlay(0, 1);
+    expect(fn).toBeInstanceOf(Function);
+    const dispatch = spy();
+    fn(dispatch);
+    expect(dispatch.calledWith({
+      type: actions.showOverlay.type, 
+      payload: {
+        resultIndex: 0,
+        excerptIndex: 1,
+      }
+    })).toBe(true);
+  });
+
+  it('should hideResultOverlay should create hideResultOverlay action', () => {
+    const fn = actions.hideResultOverlay();
+    expect(fn).toBeInstanceOf(Function);
+    const dispatch = spy();
+    fn(dispatch);
+    expect(dispatch.calledWith({
+      type: actions.hideOverlay.type, 
+    })).toBe(true);
+  });
+
+  it('should saveResultsToFile should create saveResultsToFile action', () => {
+    const fn = actions.saveResultsToFile();
+    expect(fn).toBeInstanceOf(Function);
+    const dispatch = spy();
+    const getState = () => ({ 
+      searches: {
+        searches: []
+      },
+      results: {
+        items: []
+      }
+    });
+    fn(dispatch, getState);
+    expect(dispatch.calledWith({
+      type: actions.exportResults.type, 
     })).toBe(true);
   });
 });
--- test/reducers/results.spec.ts
+++ test/reducers/results.spec.ts
@@ -2,20 +2,20 @@ import results, { IResults } from '../../app/reducers/results';
 import { saveResults, exportResults } from '../../app/actions/results';
 
 const emptyResultsSet: IResults = {
-  hasRun: true, 
-  items: [], 
+  hasRun: false,
+  items: [],
   overlay: {
-    show: false,
-    search: '',
-    phrase: '',
     body: '',
+    phrase: '',
+    search: '',
+    show: false,
   }
 };
 
 describe('reducers', () => {
   describe('results', () => {
     it('should handle initial state', () => {
-      expect(results(undefined, { type: 'unknown' })).toEqual({ hasRun: false, items: [] });
+      expect(results(undefined, { type: 'unknown' })).toEqual(emptyResultsSet);
     });
 
     it('should handle SAVE_RESULTS', () => {
