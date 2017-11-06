f04bad190de121e359c241d3871b4b25d23d8cf1
Matt Conrad <mmconrad@gmail.com>
2017-11-05 21:49:21 -0800

    Update typing dependencies that were conflicting

--- app/components/Searches/index.tsx
+++ app/components/Searches/index.tsx
@@ -3,13 +3,24 @@ import { RouteComponentProps } from 'react-router';
 import { MenuBanner } from './MenuBanner';
 import { SearchList } from './SearchList';
 import { IMenu } from '../../reducers/menu';
+import { ISearchesState, ISearch } from '../../reducers/searches';
 let styles = require('./Searches.scss');
 
 export interface IProps extends RouteComponentProps<any> {
   menu: IMenu,
-};
+  searches: ISearchesState,
+  addSearchesFromProfile(file: File): void,
+  exportSearches(): void,
+  addSearch(search: ISearch): void,
+  selectSearch(index: number): void,
+  setSearchAsUsed(searchIndex: number, isUsed: boolean|null): void,  
+  toggleMenuDropDown(): void,
+  updateNewSearchName(name: string): void,
+  removeSearch(index: number, name: string): void,
+}
 
-export class Searches extends React.Component<any> {
+
+export class Searches extends React.Component<IProps> {
   render() {
     return (
       <div className={styles.searchesColumn}>
